73e39b73a995cd2caa9730b3762135eaf0566269
Nihal Thakur <nihalthakur0609@gmail.com>
2023-08-06 19:14:39 +0530

    Add Initial Files

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectToMongo = require("./models/Notes")
 var cors = require('cors')
+const path = require("path");
 
 var app = express()
 app.use(cors())
@@ -9,7 +10,14 @@ const PORT = process.env.PORT||5000;
 
 app.use(express.json())
 
+// Static Files access
+app.use(express.static(path.join(__dirname,'../Frontend/build')))
+
 app.use("/", require("./routes/notes"))
+
+app.get("*", function (req,res) {
+    res.sendFile(path.join(__dirname,"../Frontend/build/index.html"))
+})
 
 
 
